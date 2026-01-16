5f93f431c8dc6fb8469e473d7f4fa392e5738347
Yimin2 <gownfla14@naver.com>
2026-01-16 10:40:27 +0900

    create photo delete

--- src/main/java/com/ll/galleyback/service/PhotoService.java
+++ src/main/java/com/ll/galleyback/service/PhotoService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ll.galleyback.dto.PhotoRequest;
 import com.ll.galleyback.dto.PhotoResponse;
@@ -19,7 +20,6 @@ public class PhotoService {
     private final PhotoRepository photoRepository;
     private final FileStorageService fileStorageService;
 
-
     public List<PhotoResponse> getAllPhotos() {
         return photoRepository.findAll().stream()
                 .map(PhotoResponse::fromEntity)
@@ -27,18 +27,30 @@ public class PhotoService {
     }
 
     public PhotoResponse getPhoto(Long id) {
-        Photo photo = photoRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Photo not found"));
+        Photo photo = findByid(id);
         return PhotoResponse.fromEntity(photo);
     }
 
     @Transactional
-    public PhotoResponse createPhoto(PhotoRequest photoRequest) {
+    public PhotoResponse savePhoto(PhotoRequest photoRequest, MultipartFile file) {
+        String imageUrl = fileStorageService.upload(file);
         Photo photo = Photo.builder()
                 .title(photoRequest.getTitle())
                 .description(photoRequest.getDescription())
+                .imageUrl(imageUrl)
                 .build();
         photoRepository.save(photo);
         return PhotoResponse.fromEntity(photo);
+    }
+
+    @Transactional
+    public void deletePhoto(Long id) {
+        Photo photo = findByid(id);
+        fileStorageService.delete(photo.getImageUrl());
+        photoRepository.delete(photo);
+    }
+
+    private Photo findByid(Long id) {
+        return photoRepository.findById(id).orElseThrow(() -> new RuntimeException("Photo not found"));
     }
 }
