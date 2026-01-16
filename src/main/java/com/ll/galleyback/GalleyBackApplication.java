fd0a6f734b32e1fa9545fcd90c71569c780bdd68
Yimin2 <gownfla14@naver.com>
2026-01-16 09:14:42 +0900

    first commit

---
+++ src/main/java/com/ll/galleyback/GalleyBackApplication.java
@@ -0,0 +1,13 @@
+package com.ll.galleyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleyBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalleyBackApplication.class, args);
+    }
+
+}
